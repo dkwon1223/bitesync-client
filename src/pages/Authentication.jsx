ef4bf52e1a3d14b6fdbb9a5ae022550d7a12e860
David Kwon <david.kwon@clover.com>
2025-01-02 08:00:29 -0700

    Connect form state with input changes and submit events

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -12,21 +12,26 @@ const Authentication = () => {
         const { name, value } = e.target;
         setFormData((prevData) => ({...prevData, [name]: value}));
     };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    };
 
   return (
     <section className='flex flex-col justify-center items-center w-full h-full'>
         {signup ? 
-        <form className='flex flex-col items-center h-1/2'>
+        <form className='flex flex-col items-center h-1/2' onSubmit={handleSubmit}>
             <h1 className='font-bold text-xl'>Sign Up</h1>
-            <input type="text" value="" placeholder="email" className='border'/>
-            <input type="text" value="" placeholder="password" className='border'/>
+            <input type="text" name="email" value={formData.email} onChange={handleChange} placeholder="email" className='border'/>
+            <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="password" className='border'/>
             <button type='submit' className='border bg-blue-100'>Sign Up</button>
         </form>
         :
-        <form className='flex flex-col items-center h-1/2'>
+        <form className='flex flex-col items-center h-1/2' onSubmit={handleSubmit}>
             <h1 className='font-bold text-xl'>Log In</h1>
-            <input type="text" value="" placeholder="email" className='border'/>
-            <input type="text" value="" placeholder="password" className='border'/>
+            <input type="text" name="email" value={formData.email} onChange={handleChange} placeholder="email" className='border'/>
+            <input type="password" name="password" value={formData.password} onChange={handleChange}  placeholder="password" className='border'/>
             <p>Don't have an account? <button type='button' className='border bg-blue-100' onClick={() => setSignup(true)}>Sign Up</button></p>
             <button type='submit' className='border bg-blue-100'>Log In</button>
         </form>
