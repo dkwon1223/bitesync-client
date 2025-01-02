04ed85bd950dd5726c203378f96b235e4b77ece4
David Kwon <david.kwon@clover.com>
2025-01-02 14:31:11 -0700

    Incorporate toastify feedback for signup

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,30 +1,43 @@
-import { useState } from "react"
+import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
+import { toast, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const useSignup = () => {
-    const { dispatch } = useAuthContext();
-    const [signupLoading, setSignupLoading] = useState(null);
-    const [signupError, setSignupError] = useState(null);
+  const { dispatch } = useAuthContext();
+  const [signupLoading, setSignupLoading] = useState(null);
 
-    const signupUser = async (userCredentials) => {
-        try {
-            const response = await fetch("http://localhost:8080/api/user/signup", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(userCredentials)
-            });
-            const data = await response.json();
-            if(!response.ok) {
-                throw Error(data.message[0]);
-            } 
-            localStorage.setItem('token', "wow")
-            dispatch({ type: 'LOGIN', payload: "wow" })
-            setSignupLoading(false);
-        } catch (err) {
-            setSignupError(await err.message);
-        }
+  const signupUser = async (userCredentials) => {
+    try {
+      const response = await fetch("http://localhost:8080/api/user/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userCredentials),
+      });
+      if (!response.ok) {
+        const data = await response.json();
+        const message = await data.message[0];
+        throw new Error(message);
+      }
+      toast.success("Signed up successfully ✅", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        transition: Slide,
+      });
+      setTimeout(() => {
+        location.reload();
+      }, 3000);
+    } catch (err) {
+      toast.error(err.message, {
+        position: "top-center",
+      });
     }
-    return { signupUser, signupLoading, signupError }
-}
+  };
+  return { signupUser, signupLoading };
+};
--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -27,9 +27,9 @@ const Authentication = () => {
 
   return (
     <section className='flex flex-col justify-center items-center w-full h-full'>
-        <ToastContainer />
         {signingup ? 
         <form className='flex flex-col items-center h-1/2' id='signupForm' onSubmit={handleSubmit}>
+            <ToastContainer />
             <h1 className='font-bold text-xl'>Sign Up</h1>
             <input type="text" name="email" value={formData.email} onChange={handleChange} placeholder="email" className='border'/>
             <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="password" className='border'/>
@@ -37,6 +37,7 @@ const Authentication = () => {
         </form>
         :
         <form className='flex flex-col items-center h-1/2' id='loginForm' onSubmit={handleSubmit}>
+            <ToastContainer />
             <h1 className='font-bold text-xl'>Log In</h1>
             <input type="text" name="email" value={formData.email} onChange={handleChange} placeholder="email" className='border'/>
             <input type="password" name="password" value={formData.password} onChange={handleChange}  placeholder="password" className='border'/>
