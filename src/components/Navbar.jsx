6c28167ba4d9da69e3fe46969743d89038afbaa9
David Kwon <david.kwon@clover.com>
2025-01-04 21:06:25 -0700

    Make navbar header responsive with hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+
 import { NavLink, useNavigate } from "react-router-dom";
 import { authNavLinks, noAuthNavLinks } from "../data/navLinks";
 import { useAuthContext } from "../hooks/useAuthContext";
@@ -12,76 +13,74 @@ const Navbar = () => {
   const { token } = useAuthContext(AuthContext);
   const { logoutUser } = useLogout();
   const navigate = useNavigate();
-  
+
   return (
-    <nav className="h-full w-full flex flex-col justify-start items-start space-y-6 pt-4 text-white font-sans">
-      <div
-        className="flex justify-center items-center h-12 w-[90%] cursor-pointer"
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        <img src={AppIcon} alt="BiteSync logo" className="w-1/4" />
-        <h1 className="font-extrabold text-xl w-3/4 text-center">BiteSync</h1>
-      </div>
-      <>
-        {token
-          ? authNavLinks.map((link) => {
-              return (
-                <NavLink
-                  to={link.route}
-                  key={link.label}
-                  className={({ isActive }) =>
-                    isActive
-                      ? "px-2 flex items-center space-x-2 py-2 text-white bg-[#4f46e5] rounded-md w-full shadow-md"
-                      : "px-2 flex items-center space-x-2 py-2 text-gray-300 hover:text-white hover:bg-[#4f46e5] rounded-md w-full"
-                  }
-                >
-                  <img
-                    src={link.icon} 
-                    alt={`${link.label} icon`}
-                    className="w-6 h-6 object-contain" 
-                  />
-                  <span className="text-lg font-thin">{link.label}</span>
-                </NavLink>
-              );
-            })
-          : noAuthNavLinks.map((link) => {
-              return (
-                <NavLink
-                  to={link.route}
-                  className={({ isActive }) =>
-                    isActive
-                      ? "px-2 flex items-center space-x-2 py-2 text-white bg-[#4f46e5] rounded-md w-full shadow-md"
-                      : "px-2 flex items-center space-x-2 py-2 text-gray-300 hover:text-white hover:bg-[#4f46e5] rounded-md w-full"
-                  }
-                  key={link.label}
-                >
-                  <img
-                    src={link.icon} 
-                    alt={`${link.label} icon`}
-                    className="w-6 h-6 object-contain" 
-                  />
-                  <span className="text-lg font-thin">{link.label}</span>
-                </NavLink>
-              );
-            })}
-      </>
-      {token && (
-        <NavLink
-          to="/"
-          className="flex items-center space-x-2 py-2 text-white hover:bg-[#4f46e5] rounded-md w-full"
-          key="logout"
-          onClick={logoutUser}
+    <nav className="h-full w-full flex flex-col justify-start items-start space-y-6 pt-4 text-white font-sans lg:flex lg:space-y-0">
+      <div className="flex justify-between items-center w-full px-4 lg:w-auto lg:px-0">
+        <div
+          className="flex justify-center items-center h-12 w-[90%] cursor-pointer"
+          onClick={() => navigate("/")}
         >
-          <img
-            src={logoutIcon} 
-            alt={`logout icon`}
-            className="w-6 h-6 object-contain" 
-          />
-          <span className="text-lg font-thin">Log Out</span>
-        </NavLink>
-      )}
+          <img src={AppIcon} alt="BiteSync logo" className="w-1/4" />
+          <h1 className="font-extrabold text-xl w-3/4 text-center lg:text-left">
+            BiteSync
+          </h1>
+        </div>
+      </div>
+        <>
+          {token
+            ? authNavLinks.map((link) => (
+                <NavLink
+                  to={link.route}
+                  key={link.label}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "px-2 flex items-center space-x-2 py-2 text-white bg-[#4f46e5] rounded-md w-full shadow-md"
+                      : "px-2 flex items-center space-x-2 py-2 text-gray-300 hover:text-white hover:bg-[#4f46e5] rounded-md w-full"
+                  }
+                >
+                  <img
+                    src={link.icon}
+                    alt={`${link.label} icon`}
+                    className="w-6 h-6 object-contain"
+                  />
+                  <span className="text-lg font-thin">{link.label}</span>
+                </NavLink>
+              ))
+            : noAuthNavLinks.map((link) => (
+                <NavLink
+                  to={link.route}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "px-2 flex items-center space-x-2 py-2 text-white bg-[#4f46e5] rounded-md w-full shadow-md"
+                      : "px-2 flex items-center space-x-2 py-2 text-gray-300 hover:text-white hover:bg-[#4f46e5] rounded-md w-full"
+                  }
+                  key={link.label}
+                >
+                  <img
+                    src={link.icon}
+                    alt={`${link.label} icon`}
+                    className="w-6 h-6 object-contain"
+                  />
+                  <span className="text-lg font-thin">{link.label}</span>
+                </NavLink>
+              ))}
+        </>
+        {token && (
+          <NavLink
+            to="/"
+            className="flex items-center space-x-2 py-2 text-white hover:bg-[#4f46e5] rounded-md w-full"
+            key="logout"
+            onClick={logoutUser}
+          >
+            <img
+              src={logoutIcon}
+              alt={`logout icon`}
+              className="w-6 h-6 object-contain"
+            />
+            <span className="text-lg font-thin">Log Out</span>
+          </NavLink>
+        )}
     </nav>
   );
 };
--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,17 +1,44 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { XMarkIcon, Bars3Icon } from "@heroicons/react/24/outline";
 
 const RootLayout = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <section className="w-screen h-screen flex">
-      <header className="w-[15%] h-full px-4 py-4 bg-indigo-950">
+      <button
+        className="lg:hidden fixed top-4 left-4 z-50 bg-indigo-950 p-2 rounded-md text-white focus:outline-none"
+        onClick={toggleMenu}
+      >
+        {isMenuOpen ? (
+          <XMarkIcon className="w-6 h-6" />
+        ) : (
+          <Bars3Icon className="w-6 h-6" />
+        )}
+      </button>
+      <header
+        className={`${
+          isMenuOpen ? "block w-[35%]" : "hidden"
+        } lg:block w-[15%] lg:w-[15%] h-full px-4 py-4 bg-indigo-950`}
+      >
         <Navbar />
       </header>
-      <main className="w-[85%] h-full bg-[#F6FAFE]">
-        <Outlet/>
+      <main
+        className={`${
+          isMenuOpen ? "w-full lg:w-[85%]" : "w-full lg:w-[85%]"
+        } h-full bg-[#F6FAFE] transition-all duration-300`}
+      >
+        <Outlet />
       </main>
     </section>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
+
