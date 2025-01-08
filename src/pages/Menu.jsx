0a8e9e3bdbe9695287275f8777edef4682f113c7
David Kwon <david.kwon@clover.com>
2025-01-08 08:09:58 -0700

    Update Menu page UI for unavailable items

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -153,18 +153,20 @@ const Menu = () => {
           >
             <div className="flex w-full items-center justify-between space-x-6 p-6">
               <div className="flex-1 truncate">
-                <div className="flex items-center space-x-3">
+                <div className="flex items-center justify-between space-x-3">
                   <h3 className="truncate text-sm font-medium text-gray-900">
                     {item.name}
                   </h3>
                   <span className="inline-flex shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
                     {item.category}
                   </span>
+                  <img
+                    alt={`${item.name}`}
+                    src={item.imageUrl}
+                    className="size-14 shrink-0 rounded-full bg-gray-300 object-cover"
+                  />
                 </div>
-                <p className="mt-1 truncate text-sm text-gray-500 my-8">
-                  {item.description}
-                </p>
-                <p className="my-4 flex justify-between items-center">
+                <p className="my-4 flex justify-between items-center text-sm">
                   Available:{" "}
                   {item.available ? (
                     <CheckCircleIcon
@@ -184,18 +186,18 @@ const Menu = () => {
                     { label: "Ingredient Cost", value: item.costToMake },
                     {
                       label: "Gross Profit",
-                      value: item.price - item.costToMake,
+                      value: item.costToMake === 0 ? 0 : item.price - item.costToMake,
                     },
                   ].map((entry, index) => (
                     <div key={`id-${item.id}-name-${item.name}${index}-label`} className="contents">
-                      <span className="text-gray-800">
+                      <span className="text-gray-800 text-xs w-1/2">
                         {entry.label}:
                       </span>
                       <span
                         key={`id-${item.id}-name-${item.name}${index}-value`}
-                        className="text-right font-medium text-gray-700"
+                        className="text-right font-medium text-gray-700 text-xs"
                       >
-                        {Intl.NumberFormat("en-US", {
+                        {entry.value === 0 ? "N/A" : Intl.NumberFormat("en-US", {
                           style: "currency",
                           currency: "USD",
                         }).format(entry.value)}
@@ -204,11 +206,6 @@ const Menu = () => {
                   ))}
                 </div>
               </div>
-              <img
-                alt={`${item.name}`}
-                src={item.imageUrl}
-                className="size-20 shrink-0 rounded-full bg-gray-300 object-cover"
-              />
             </div>
             <div>
               <div className="-mt-px flex divide-x divide-gray-200">
